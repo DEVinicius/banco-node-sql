8ef3143243c0b5bb5cab3005833ce062dd3f5788
Vinicius Pereira de Oliveira <agavi2014@hotmail.com>
2020-05-24 15:27:29 -0300

    inserção da migration user

--- src/database/migrations/20200524181709-create-users.js
+++ src/database/migrations/20200524181709-create-users.js
@@ -3,7 +3,30 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
 
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+      return queryInterface.createTable('users', { 
+        id:{
+          type: Sequelize.INTEGER,
+          primaryKey: true,
+          autoIncrement: true,
+          allowNull: false
+        },
+        name: {
+          type: Sequelize.STRING,
+          allowNull: false
+        },
+        email: {
+          type: Sequelize.STRING,
+          allowNull: false
+        },
+        created_at:{
+          type: Sequelize.DATE,
+          allowNull: false
+        },
+        updated_at: {
+          type: Sequelize.DATE,
+          allowNull: false
+        }
+      });
     
   },
 
